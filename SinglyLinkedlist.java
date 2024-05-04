3b6c9ed141d854085372ec9fffc8bcf8d6256dc2
Krish Zinzuvadiya <168444725+Krish-zinzuvadiya@users.noreply.github.com>
2024-05-04 20:10:55 +0530

    Update SinglyLinkedlist.java

--- SinglyLinkedlist.java
+++ SinglyLinkedlist.java
@@ -8,10 +8,13 @@ public class SinglyLinkedlist {
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
             System.out.println("1. Insert First Element.");
             System.out.println("2. Insert Last Element.");
-            System.out.println("3. Delete First Element.");
-            System.out.println("4. Delete Last Element.");
-           // System.out.println("5. Display.");
-            System.out.println("5. Exit.");
+            System.out.println("3. Search The Element.");
+            System.out.println("4. Insert Before Any Value.");
+            System.out.println("5. Insert After Any Value.");
+            System.out.println("6. Delete First Element.");
+            System.out.println("7. Delete Last Element.");
+            System.out.println("8. Delete Any Particular Value.");
+            System.out.println("9. Exit.");
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
             System.out.print("Enter Your Choice : ");
             int choice = sc.nextInt();
@@ -22,26 +25,62 @@ public class SinglyLinkedlist {
                     s.insertFirst(y);
                     s.display();
                     break;
+
                 case 2:
                     System.out.print("Enter The Element : ");
                     int x = sc.nextInt();
                     s.insertLast(x);
                     s.display();
                     break;
-                case 3:
+
+                case 3: 
+                    System.out.print("Enter The Element : ");
+                    int z = sc.nextInt();
+                    s.search(z);
+                    break;
+
+                case 4:
+                    System.out.print("Enter The Element From LinkList : ");
+                    int val1 = sc.nextInt();
+                    System.out.print("Enter The Element Before "+ val1 + " : ");
+                    int d1 = sc.nextInt();
+                    s.insertBeforeValue(d1, val1);
+                    s.display();
+                    break;
+
+                case 5:
+                System.out.print("Enter The Element From LinkList : ");
+                int val2 = sc.nextInt();
+                System.out.print("Enter The Element After "+ val2 + " : ");
+                int d2 = sc.nextInt();
+                s.insertAfterValue(d2, val2);
+                s.display();
+                break;
+
+                case 6:
                     s.deleteFirst();
                     s.display();
                     break;
-                case 4:
+
+                case 7:
                     s.deleteLast();
                     s.display();
                     break;
-                case 5: 
+
+                    case 8: 
+                        System.out.print("Enter Particular Element That You Want To Delete : ");
+                        int val3 = sc.nextInt();
+                        s.deleteParticularValue(val3);
+                        s.display();
+                        break;
+                case 9: 
+                    System.out.println("Thank You For Using Our Code...");
                     System.exit(0);
                     break;
 
                 default:
                     System.out.println("Invalid Choice.");
+                    System.out.println("Please Enter Valid (1-9) Choice...");
             }
         }
     }
@@ -85,6 +124,80 @@ class SinglyLL {
         }
     }
 
+    void search(int val){
+        Node temp = first;
+        int f = 0;
+        int c = 0;
+        while(temp != null){
+            c++;
+            if(temp.data == val){
+                System.out.println(val+" Value is founded at --> "+c);
+                f = 1;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(f == 0){
+            System.out.println(val+" Value is not found");
+        }
+    }
+
+    void insertBeforeValue(int d, int val){
+        Node temp = first;
+        int f =0;
+        int c = 0;
+        while(temp != null){
+            c++;
+            if(temp.data == val){
+                System.out.println(val+" Value is founded at --> "+c);
+                f = 1;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(f == 0){
+            System.out.println(val+" Value is not found");
+        } else {
+            Node n = new Node(d);
+            if(first.data == val){
+                n.next = first;
+                first = n;
+            } else {
+                Node temp1 = first;
+                while(temp1.next.data!= val){
+                    temp1 = temp1.next;
+                }
+                n.next = temp1.next;
+                temp1.next = n;
+            }
+        }
+    }
+    void insertAfterValue(int d, int val){
+        Node temp = first;
+        int f =0;
+        int c = 0;
+        while(temp != null){
+            c++;
+            if(temp.data == val){
+                System.out.println(val+" Value is founded at --> "+c);
+                f = 1;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(f == 0){
+            System.out.println(val+" Value is not found");
+        } else {
+            Node n = new Node(d);
+            
+                Node temp1 = first;
+                while(temp1.data!= val){
+                    temp1 = temp1.next;
+                }
+                n.next = temp1.next;
+                temp1.next = n;
+        }
+    }
     void deleteFirst(){
         if(first == null){
             System.out.println("List is Empty.");
@@ -107,6 +220,36 @@ class SinglyLL {
             temp.next = null;
         }
         System.out.println("Last Node Deleted.");
+    }
+
+    void deleteParticularValue(int val){
+        Node temp = first;
+        int f =0;
+        int c = 0;
+        while(temp != null){
+            c++;
+            if(temp.data == val){
+                System.out.println(val+" Value is founded at --> "+c);
+                f = 1;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(f == 0){
+            System.out.println(val+" Value is not found");
+        } else {
+            if(first.data == val){
+                first = first.next;
+            } else {
+                Node temp1 = first;
+                while(temp1.next.data!= val){
+                    temp1 = temp1.next;
+                }
+                Node a = temp1.next;
+                temp1.next = a.next;
+                a.next = null;
+            }
+        }
     }
 
     void display(){
